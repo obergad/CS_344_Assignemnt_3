ebb4aded8a115185e7ce4a910e566ec0fe755485
obergad <obergad@oregonstate.edu>
2020-10-30 12:39:17 -0700

    started on parsing the cmd line

--- smallsh.c
+++ smallsh.c
@@ -31,16 +31,32 @@ This program will...
 struct cmd_line
 {
   char* command; //Initial command from the user
-  char** argList;//List of extra arguments from the user
-  int argNum; //Number of arguments
+  char** argList;//List of extra arguments from the user.
+  int numArg; // Ammount of arguments
   char* inputFile;//File path for the input file
   char* outputFile;//File Path for the output file
-  int bgProcess; //TF value 1 TRUE or 0 FALSE
+  int bgProcess; //TF value 1 TRUE or 0 FALSE || If '&' char is detected properly then is TRUE
 };
 
+/*
+  Fucntion: parse_CMD
+  Description: Pareses the users input into the struct cmd_line
+  Input: char* of the userInput
+  output: struct* cmd_line
+
+*/
 struct cmd_line *parse_CMD(char* userInput){
-    struct cmd_line* cmdInput;
-    cmdInput->bgProcess = 0;
+    struct cmd_line* cmdInput = malloc(sizeof(struct cmdLine));
+    char buffer = " ";
+    char *saveptr;
+
+    char *token = strtok_r(userInput, buffer, &saveptr); //Take the first word in the line
+    cmdInput->command = calloc(strlen(token) + 1, sizeof(char)); //Allocate space in the struct
+    strcpy(cmdInput->command, token);//Coppy the token into the struct
+    
+
+
+
 
     return cmdInput;
 }
@@ -62,57 +78,64 @@ void start_shell(){
     printf("\n");
     sleep(1);
 }
+
 /*
   Function: cmd_prompt
   Description: simply prompts the command prompt for the user and takes in a value
   Paramaters: NULL
-  Return: A string that is the input of the user.
+  Return: A string that is the input from the user.
 */
 char* cmd_prompt(){
   char* userInput;
-  ssize_t bytesRead;
-
-
-
-
-
-  printf("test");
-  userInput = calloc(MAXCHAR + 1, sizeof(char));
-  printf(": ");
-
-  // Open the specified file for reading only
-  // FILE *movieFile = fopen(filePath, "r");
-
   char *currLine = NULL;
-  size_t len = 0;
-  int nread;
-  // char *dirName;
-  // char *token;
-  int linenum = 1; //Use to skip the firt line of text
+  size_t max = MAXCHAR;
+  int nread; //Number of bytes read by the getline function.
 
-  // nread = getline(&currLine, &len, FILE* stream);
+  userInput = calloc(MAXCHAR + 1, sizeof(char)); //Allocating space for userinput and clear userInput
+  printf("\n: "); //Beginging of a newline in the command prompt.
+  fflush(stdout);
+  nread = getline(&currLine, &max , stdin); //stdin reads from the commandline
   userInput = currLine;
+
+  if(nread > MAXCHAR){
+    printf("Error: Max command length has been reached\n");
+    fflush(stdout);
+    exit(EXIT_FAILURE);
+  }
+
+  sleep(1);
+  printf("\n");
+  fflush(stdout);
   printf("**Success**\nYour CMD line argument is: %s\n", userInput);
-  printf("After Loop userInput: %s\n", userInput);
+  fflush(stdout);
   return userInput;
 }
 
 int main(int argc, char const *argv[]) {
-  start_shell();
+    char* cmdLine = NULL;
+    start_shell();
+    while (strcmp(cmdLine,"___EOF___") == 0) {
+      strcpy(cmdLine,cmd_prompt());
+    }
+    int i = 0;
+    printf("Exiting");
+    fflush(stdout);
+    while (i != 3) {
+      printf(".");
+      sleep(1);             //This is for the meme
+      fflush(stdout);
+      i++;
+    }
 
-
-    char *newargv[] = { "/bin/ls", "-al", NULL };
-    execv(newargv[0], newargv);
+    //
+    // char *newargv[] = { "/bin/ls", "-al", NULL };
+    // execv(newargv[0], newargv);
     /* exec returns only on error */
-    perror("execv");
+    // perror("execv");
+    // exit(EXIT_FAILURE);
 
 
 
-    exit(EXIT_FAILURE);
-
-
-
-  // cmd_prompt();
 
 
 
