7db59a217d408a6332c7c7e3b17c753bdc08ecb4
obergad <obergad@oregonstate.edu>
2020-11-04 21:55:40 -0800

    ITS KIND OF ALMOST MOSTLY WORKING THANK YOU EVERYONE FOR YOUR AMAZING HE

--- smallsh.c
+++ smallsh.c
@@ -34,10 +34,16 @@ This program will...
 #define PIDCHAR '$'
 
 
-int CATCH_STGTSPT = 0; // For catching SIGTSTP
+int CATCH_STGTSPT = 1; // For catching SIGTSTP
 int pid;
 // int fgMode = 1; //If fgMode is 1 that means that forground only mode is on otherwise it is not.
 int childProcess_id = 0;
+
+struct pid_str{
+  int pidIns;
+  struct pid_str* pidnext;
+};
+struct pid_str* my_pid;
 
 struct cmd_line
 {
@@ -151,26 +157,32 @@ struct cmd_line *parse_CMD(char* userInput){
           fflush(stdout);
           token = strtok_r(NULL ," ", &saveptr); //Take each arg  of the the line and token it
 
-        //Detect background process
-        if (strncmp(cmdInput->argList[i], "&", strlen("&")) == 0) {
-          cmdInput->bgProcess = 1;
-          cmdInput->argList[i] = NULL;
-      }
+
       cmdInput->numArg = i;
       i++;
     }
     i = cmdInput->numArg + 1;
     cmdInput->argList[i] = NULL;
 
+    //-----------------------------------------------------------------------------------------
+    //Detect background process
+    //-----------------------------------------------------------------------------------------
+    i = cmdInput->numArg;
+    if (strncmp(cmdInput->argList[i], "&", strlen("&")) == 0 ) {
+      cmdInput->bgProcess = 1;
+      cmdInput->argList[i] = NULL;
+      // printf("cmdInput->argList[0]:%s\n", cmdInput->argList[0] );
+      // printf("cmdInput->argList[1]:%s\n", cmdInput->argList[1] );
+      // printf("cmdInput->argList[2]:%s\n", cmdInput->argList[2] );
+    }
     i = 0;
     // Check for > to find file input
     while ( i < cmdInput->numArg) {
+
       //-----------------------------------------------------------------------------------------
       // Check for > to find file input
       //-----------------------------------------------------------------------------------------
       if(strncmp( cmdInput->argList[i], "<", strlen("<")) == 0) {
-        printf("Found <\n");
-        printf("cmdInput->argList[i + 1]: %s\n", cmdInput->argList[i + 1]);
         strcpy(cmdInput->inputName, cmdInput->argList[i + 1]);
         cmdInput->argList[i] = NULL;
         cmdInput->argList[i + 1] = NULL;
@@ -180,8 +192,6 @@ struct cmd_line *parse_CMD(char* userInput){
       // Check for > to find file output
       //-----------------------------------------------------------------------------------------
       else if(strncmp( cmdInput->argList[i], ">", strlen(">")) == 0){
-        printf("Found >\n");
-        printf("cmdInput->argList[i + 1]: %s\n", cmdInput->argList[i + 1]);
         strcpy(cmdInput->outputName, cmdInput->argList[i + 1]);
         cmdInput->argList[i] = NULL;
         cmdInput->argList[i + 1] = NULL;
@@ -201,6 +211,7 @@ struct cmd_line *parse_CMD(char* userInput){
 */
 void run_status(){
     //code based on code shown in lecture / modules
+
     if(WIFEXITED(childProcess_id)){
       printf("exited normally with status %d\n", WEXITSTATUS(childProcess_id));
       fflush(stdout);
@@ -211,9 +222,54 @@ void run_status(){
     }
 }
 
+void bg_run_check(){
+  pid_t pid;
+  struct pid_str* temp = my_pid;
+  while (temp != NULL) {
+      pid =  waitpid(temp->pidIns, &childProcess_id, WNOHANG);
+      if(pid > 0){
+        if(WIFEXITED(childProcess_id)){
+          printf("pid: %i exited normally with status %d\n", pid ,WEXITSTATUS(childProcess_id));
+          fflush(stdout);
+        }
+        else{
+          printf("pid: %i exited not normally due to signal %d\n",pid,  WTERMSIG(childProcess_id));
+          fflush(stdout);
+        }
+      }
+      temp = temp->pidnext;
+    }
+}
 
+// Code in this function interpereted from https://repl.it/@cs344/53singal2c
+//Function: SIGINT_handler
+//Input: int signo
+//Output: NULL
+void SIGINT_handler(int signo){
+  char* message = "Caught SIGINT, sleeping for 10 seconds\n";
+  // We are using write rather than printf
+	write(STDOUT_FILENO, message, 39);
+	sleep(10);
+}
+// Code in this function interpereted from https://repl.it/@cs344/53singal2c
+//Function: SIGTSTP_handler
+//Input: int signo
+//Output: NULL
+void SIGTSTP_handler(int signo){
 
-
+  if( signo == SIGTSTP){
+      if (CATCH_STGTSPT == 1){
+        CATCH_STGTSPT = 0;
+        char* helpmeplease = "in SIGTSTP_handler\n";
+        write(STDIN_FILENO, helpmeplease, 19);
+      }
+      else{
+        CATCH_STGTSPT = 1;
+        char* helpmeplease = "in SIGTSTP_handler\n";
+        write(STDIN_FILENO, helpmeplease, 19);
+      }
+  }
+}
 /*
     forks the current procrss and runs the command as a child
 */
@@ -229,18 +285,15 @@ int run_Command(struct cmd_line *cmdInput, struct sigaction sigint_handler, stru
     }
     //--------------------------------------------------------
     //CD
-    if(strcmp(cmdInput->argList[0], "cd ") == 0) {
+    if(strcmp(cmdInput->argList[0], "cd") == 0 && cmdInput->argList[1] == NULL) {
       chdir(getenv("HOME"));
       getcwd(currDir, size);
-      printf("You are now in: %s: \n",currDir);
       waitpid(childStatus, &childProcess_id, 0 );
       return 1;
     }
     else if(strcmp(cmdInput->argList[0], "cd") == 0){
-      printf("Executing cd with argument '%s'\n", cmdInput->arg);
       chdir(cmdInput->argList[1]);
       getcwd(currDir, size);
-      printf("You are now in: %s: \n",currDir);
       waitpid(childStatus, &childProcess_id, 0 );
       return 1;
     }
@@ -256,13 +309,13 @@ int run_Command(struct cmd_line *cmdInput, struct sigaction sigint_handler, stru
     //Run Status
     //--------------------------------------------------------
     //Sleep
-    else if(strncmp(cmdInput->argList[0], "sleep", strlen("sleep")) == 0){
-      //
-      // if (cmdInput->bgProcess == 1) {
-      // }
-      // else{
-      //   execvp(cmdInput->argList[0], cmdInput->argList);
-    }
+    // else if(strncmp(cmdInput->argList[0], "sleep", strlen("sleep")) == 0){
+    //   //
+    //   // if (cmdInput->bgProcess == 1) {
+    //   // }
+    //   // else{
+    //   //   execvp(cmdInput->argList[0], cmdInput->argList);
+    // }
     //Sleep
     // --------------------------------------------------------
     else{
@@ -280,11 +333,8 @@ int run_Command(struct cmd_line *cmdInput, struct sigaction sigint_handler, stru
              sigint_handler.sa_handler = SIG_DFL;
              sigaction(SIGINT, &sigint_handler, NULL);
            }
-
           if (strcmp(cmdInput->inputName, "")) {
             // open the file
-            printf("cmdInput->inputName:%s.\n", cmdInput->inputName );
-            fflush(stdout);
             input = open(cmdInput->inputName, O_RDONLY, 0644);
             if (input == -1) {
               perror("Unable to open the input file\n");
@@ -303,8 +353,6 @@ int run_Command(struct cmd_line *cmdInput, struct sigaction sigint_handler, stru
           // Handle output, code is basically straight from https://repl.it/@cs344/54redirectc#main.c
           if (strcmp(cmdInput->outputName, "")) {
             // open the file
-            printf("cmdInput->outputName: %s \n", cmdInput->outputName );
-            fflush(stdout);
             output = open(cmdInput->outputName, O_WRONLY | O_CREAT | O_TRUNC , 0644);
             if (output == -1) {
               perror("Unable to open output file\n");
@@ -320,6 +368,13 @@ int run_Command(struct cmd_line *cmdInput, struct sigaction sigint_handler, stru
             fcntl(output, F_SETFD, FD_CLOEXEC);
           }
            fflush(stdout);
+           SIGTSTP_handler(spawnPid);
+           // if (strcmp(cmdInput->argList[0], "sleep")) {
+           //   printf("b4 execvp cmdInput->argList[0]: %s\n",cmdInput->argList[0]);
+           //   printf("b4 execvp cmdInput->argList[1]: %s\n",cmdInput->argList[1]);
+           // }
+
+           fflush(stdout);
            execvp(cmdInput->argList[0], cmdInput->argList);
            // exec only returns if there is an error
            perror("execvp");
@@ -327,11 +382,15 @@ int run_Command(struct cmd_line *cmdInput, struct sigaction sigint_handler, stru
            break;
         default:
            // In the parent process
-           if (cmdInput->bgProcess == 1) {
+           if (cmdInput->bgProcess == 1 && CATCH_STGTSPT == 1) {
+             struct pid_str* new_pid = (struct pid_str*)malloc(sizeof(struct pid_str));
+             new_pid->pidIns = spawnPid;
+             new_pid->pidnext = my_pid;
+             my_pid = new_pid;
+
              printf("Background PID is: %i\n", spawnPid );
              fflush(stdout);
-             waitpid(childStatus, &childProcess_id, WNOHANG);
-             spawnPid = waitpid(spawnPid, &childStatus, 0);
+             spawnPid = waitpid(spawnPid, &childStatus, WNOHANG);
            }
            else{
             // Wait for child's termination
@@ -383,59 +442,30 @@ char* cmd_prompt(){
 
 
 
-// Code in this function interpereted from https://repl.it/@cs344/53singal2c
-//Function: sigint_handler
-//Input: int signo
-//Output: NULL
-void sigint_handler(int signo){
-  char* message = "Caught SIGINT, sleeping for 10 seconds\n";
-  // We are using write rather than printf
-	write(STDOUT_FILENO, message, 39);
-	sleep(10);
-}
-// Code in this function interpereted from https://repl.it/@cs344/53singal2c
-//Function: SIGTSTP_handler
-//Input: int signo
-//Output: NULL
-void SIGTSTP_handler(int signo){
-  if( signo == SIGTSTP){
-      if (CATCH_STGTSPT){
-        CATCH_STGTSPT = 0;
-      }
-      else{
-        CATCH_STGTSPT = 1;
-      }
-  }
 
-}
 
 int main(int argc, char const *argv[]) {
 
   struct sigaction sigint_handler = { { 0 } };
   struct sigaction sigtstp_handler = { { 0 } };
 
-  sigint_handler.sa_handler = SIG_IGN;
+  sigint_handler.sa_handler = SIGINT_handler;
   sigfillset(&sigint_handler.sa_mask);
-  sigint_handler.sa_flags = 0;
+  sigint_handler.sa_flags = SA_RESTART;
   sigaction(SIGINT, &sigint_handler, NULL);
 
   sigtstp_handler.sa_handler = SIGTSTP_handler;
   sigfillset(&sigtstp_handler.sa_mask);
-  sigtstp_handler.sa_flags = 0;
+  sigtstp_handler.sa_flags = SA_RESTART;
   sigaction(SIGTSTP, &sigtstp_handler, NULL);
-  //
-  // int sigaddset(&my_signal_set, signal);
-  // int sigdelset(&my_signal_set, signal);
-  // sigset_t my_signal_set;
-  // int sigemptyset(&my_signal_set);
 
 
-
+    int i = 0;
     char* cmdLine;
-    start_shell();
     printf("PID: %d\n", pid);
     do {
     cmdLine =  calloc(MAXCHAR + 1, sizeof(char));
+    bg_run_check();
     strcpy(cmdLine,cmd_prompt());
     if (strcmp(cmdLine, "\n") == 0 || strcmp(cmdLine, "\0") == 0 ) {
       continue;
